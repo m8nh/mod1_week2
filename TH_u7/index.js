eb790a415bdd57f33945d47181f37aed464d2d23
minh <minhnghiemquangminh75@gmail.com>
2022-10-10 11:16:14 +0700

    week2

---
+++ TH_u7/index.js
@@ -0,0 +1,12 @@
+let inputWidth;
+let inputHeight;
+
+// inputWidth = prompt("Enter the width");
+// inputHeight = prompt("Enter the height");
+
+inputWidth = prompt("Enter the mat");
+inputHeight = prompt("Enter the hfish");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
